3efa6454da4b98abe5ea7798fcec487f92c48e07
Nolaaaaa <1009198372@qq.com>
2018-07-27 22:19:27 +0800

    用看得见的方式画一个皮卡丘

---
+++ pikachu/dynamic/main.js
@@ -0,0 +1,131 @@
+!function(){
+    var code = ` 
+        .preview{
+            /* 设置画布的颜色 */
+            background: #fee433;
+        }
+        .nose{
+            /* 定位让鼻子居中 */
+            position: absolute;
+            left: 50%;
+            top: 28px;
+            margin-left: -12px;
+            /* 以下是画鼻子的扇形 */
+            width: 0;
+            height: 0;
+            border: 12px solid;
+            border-color: black transparent transparent;
+            border-radius: 12px;
+            /* 调整扇形的宽度 */
+            border-width: 12px;
+        }
+        .eye{
+            /* 开始画眼黑 */
+            background: black;
+            width: 49px;
+            height: 49px;
+            position: absolute;
+            border-radius: 50%;
+            border: 2px solid black;
+        }
+        .eye::before{
+            /* 画眼睛白色 */
+            position: absolute;
+            content: '';
+            width: 24px;
+            height: 24px;
+            top: 4px;
+            left: 2px;
+            border: 2px solid black;
+            border-radius: 50%;
+            background: white;
+        }
+        .eye.left{
+            right: 50%;
+            margin-right: 90px;
+        }
+        .eye.right{
+            left: 50%;
+            margin-left: 90px;
+        }
+        .face{
+            /* 开始画脸 */
+            width: 68px;
+            height: 68px;
+            border: 2px solid black;
+            background: #fc0d1c;
+            position: absolute;
+            border-radius: 50%;
+            top: 40%;
+        }
+        .face.left{
+            right: 50%;
+            margin-right: 118px;
+        }
+        .face.right{
+            left: 50%;
+            margin-left: 118px;
+        }
+        .upperLip{
+            /* 开始嘴 */
+            height: 20px;
+            width: 70px;
+            position: absolute;
+            border: 2px solid black;
+            background: #fee433;
+            /* 调整鼻子和上嘴唇的距离 */
+            top: 48px;
+        }
+        .upperLip.left{
+            border-bottom-left-radius: 40px 25px;
+            border-top: none;
+            border-right: none;
+            transform: rotate(-20deg);
+            left: 50%;
+            margin-left: -70px;      
+        }
+        .upperLip.right{
+            border-bottom-right-radius: 40px 25px;
+            border-top: none;
+            border-left: none;
+            transform: rotate(20deg);
+            right: 50%;
+            margin-right: -70px;
+        }
+        .lowerLip{
+            width: 160px;
+            height: 1600px;
+            background: #990513;
+            border-radius: 100px/600px;
+            border: 2px solid black;
+            position: absolute;
+            bottom: 0;
+            overflow: hidden;
+        }
+        .lowerLip::after{
+            /* 画舌头 */
+            background: red;
+            margin-left: -50px;
+            left: 50%;
+            border-radius: 75px;
+        }
+ 
+    `
+    writeCode('',code)
+    function writeCode(prefix,code,fn){
+        let domCode = document.querySelector('.code')
+        let styleTag = document.querySelector('.styleTag')
+        domCode.innerHTML = prefix || ''
+        let n = 0
+        let timer = setInterval(()=>{
+            n += 1
+            domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css); //页码放到HTML中同时让代码高亮
+            domCode.scrollTop = domCode.scrollHeight
+            styleTag.innerHTML = prefix + code.substring(0,n)   //code的内容放code中，文字部分注释起来
+            if (n >= code.length) {     //停止计时器
+                window.clearInterval(timer)
+                fn.call()
+            }
+        },1)
+    }
+}.call()

a4169fbcdf29bf0ab3575b1e95b3301d254e5795
Nolaaaaa <venomhmn@163.com>
2018-12-12 14:27:18 +0800

    棋子下到交叉线上，并建一个二维数组用来存下的棋

--- js-gomoku/main.js
+++ js-gomoku/main.js
@@ -10,16 +10,22 @@
     setChecker()
     
     let width = canvas.width/15
+    let height = canvas.height/15
     
 
     // 画棋盘的格子
-    var arr = new Array();
     for (let i = 0; i < 15; i++) {
-      arr[i] = new Array();
       for (let j = 0; j < 15; j++) {
-        arr[i][j] = ''
-        console.log(arr[i][j])
-        context.strokeRect(width * j, width * i, width, width);
+        context.strokeRect(width * i, height * j, width, height)
+      }
+    }
+
+    // 建一个二维数组
+    var arr = new Array();
+    for (let i = 1; i < 15; i++) {
+      arr[i] = new Array()
+      for (let j = 1; j < 15; j++) {
+        arr[i][j] = 0
       }
     }
 
@@ -27,7 +33,7 @@
     {
       // 棋子初始化，先下白棋还是黑棋，1 是白的，0是黑的ssss
       let n = 0
-      canvas.onmousedown = function (event) {
+      canvas.onmousedown = function (e) {
         if (n === 0) {
           playChess('#000000', width/2.5)
           n = n + 1
@@ -40,12 +46,6 @@
 
 
 
-
-
-
-
-
-
     /************函数部分不要看***********/
 
     // 检测所使用的设备，并确定画布的宽高
@@ -53,7 +53,7 @@
       // console.log(navigator.platform)
       var viewHeight = Math.floor(window.innerHeight||document.documentElement.clientHeight)
       var viewWidth = Math.floor(window.innerWidth||document.documentElement.clientWidth)
-      canvas.width = Math.min(viewWidth, viewHeight) - 50
+      canvas.width = Math.min(viewWidth, viewHeight) - 60
       canvas.height =  canvas.width
 
     }
@@ -61,10 +61,23 @@
     // 下棋的函数
     function playChess(fillColor, radius) {
       context.fillStyle = fillColor
-        context.strokeStyle = '#000000'
-        var x = event.layerX
-        var y = event.layerY
-        drawCircle(x, y, radius)
+      context.strokeStyle = '#000000'
+      // 点击的坐标位置
+      var x = event.layerX
+      var y = event.layerY
+      var indX = Math.round(x/width)
+      var indY = Math.round(y/height)
+      
+      console.log(arr[indX][indY])
+      if(arr[indX][indY] == 1) {
+        console.log('这里已经下过啦，换个地方试试')
+        return 
+      }
+      arr[indX][indY] = 1
+      // 让棋子下在交叉线的位置
+      x = indX * width + 4
+      y = indY * height + 4
+      drawCircle(x, y, radius)
     }
 
     // 画一个圆的函数

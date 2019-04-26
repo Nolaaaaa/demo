89a1e694cca69264f21979455f6a46fe571863fe
Nolaaaaa <1009198372@qq.com>
2019-04-26 18:07:18 +0800

    update

--- node-demo/server.js
+++ node-demo/server.js
@@ -2,9 +2,8 @@
 var http = require('http');
 
 // 2. 创建http服务器
-// 参数: 请求的回调, 当有人访问服务器的时候,就会自动调用回调函数
 var server = http.createServer(function (request, response) {
-    // 有人访问的时候调用
+    // 请求的回调, 当有人访问服务器的时候,就会自动调用回调函数
     console.log("狼来了")
     response.write('Hello World')
     response.end()
